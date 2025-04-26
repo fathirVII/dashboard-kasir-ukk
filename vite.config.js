5d4112862bd31447838831db49523f63121420d0
fathirVII <fathirnurhakim7@gmail.com>
2025-04-26 07:21:31 +0700

    ubah env ca.pen ke path yang sesuai

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,26 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import tailwindcss from "@tailwindcss/vite";
+import vue from "@vitejs/plugin-vue";
+import fs from "fs";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
         }),
         tailwindcss(),
+        vue(),
     ],
-    
+    server: {
+        https: {
+            key: fs.readFileSync(
+                "A:/TUGAS UJIKOM/dashboard_kasir/.ssl/server.key"
+            ),
+            cert: fs.readFileSync(
+                "A:/TUGAS UJIKOM/dashboard_kasir/.ssl/server.crt"
+            ),
+        },
+    },
 });

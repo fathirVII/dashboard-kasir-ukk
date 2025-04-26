a0bf3c27f4fa3b6e5d04b097637df5e1457a366b
fathirVII <fathirnurhakim7@gmail.com>
2025-04-26 07:40:47 +0700

    ubah config openssl dan path file nya

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
+import fs from "fs";
 
 export default defineConfig({
     plugins: [
@@ -10,4 +11,14 @@ export default defineConfig({
         }),
         tailwindcss(),
     ],
+    server: {
+        https: {
+            key: fs.readFileSync(
+                "/app/storage/ssl/server.key"
+            ),
+            cert: fs.readFileSync(
+                "/app/storage/ssl/server.crt"
+            ),
+        },
+    },
 });
